4cdad9bf08f46f6b69754750c89db2a18fa298bb
Giovanni7779 <55230060+Giovanni7779@users.noreply.github.com>
2019-09-18 17:27:07 +0200

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -20,7 +20,7 @@ def Fourier(S, T, F) :
 
 def Delta(T) :
   Delta = [0]*int((T[1]- T[0])/T[2]) ;
-  Delta[int(len(Delta)/2)+1] = 2 ; 
+  Delta[int(len(Delta)/2)+1] = 1 ; 
   return Delta ;
 
 def Sine(T,f1,f2) :  
@@ -30,20 +30,31 @@ def Sine(T,f1,f2) :
      Sine[i] = math.sin(Time[i]*f1) + 2*math.sin(Time[i]*f2) ;
   return Sine; 
 
+def SawTooth(T,f1,f2) :
+   SawTooth = [0]*int((T[1]- T[0])/T[2]) ;
+   Time = np.arange(T[0], T[1], T[2]) ;
+   Controll_1 = 0; 
+   for i in range (0, len(Sine) - 1) : 
+     SawTooth[i] = f1*(Time[i] - Controll_1) ;
+     if SawTooth[i] > 1 : 
+        SawTooth[i] = 0 ;
+        Controll_1 = Time[i]; 
+   return SawTooth;
 #---------------------
 
 Time = np.array([0, 1*(10**6) , 10**3     ]) ;                  #HERE VARYING STARTING STOPPIN AND STEP
-Freq = np.array([0, 10000*(10**(-5)), 10000*(10**(-8)) ]) ;     #HERE AS WELL IS THE TF SPACE
+Freq = np.array([0, 5*(10**(-5)), 5*(10**(-8)) ]) ;     #HERE AS WELL IS THE TF SPACE
   
 #Creation of the Signals
 
 Delta = Delta(Time) ;
 Sine = Sine (Time, 2*math.pi*(10**(-5)), 0 ) ;                  #HERE YOU CAN CHANGE FREQUENCY (also add another)
+SawTooth = SawTooth(Time, 10**(-5), 0 ) ;
 
 Frequency = np.arange(Freq[0], Freq[1], Freq[2]) ;              #This helps with the graphs
 Tempo = np.arange(Time[0], Time[1], Time[2]) ;                  #This helps with the graphs
 
-DATA = Sine ;                                                   #HERE TO CHANGE THE PLOT
+DATA = SawTooth ;                                                   #HERE TO CHANGE THE PLOT
 FT = Fourier(DATA, Time, Freq) ;
 
 fig = plt.figure(figsize=(20,12))
