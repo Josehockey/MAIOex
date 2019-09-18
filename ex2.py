28ad39d907c4589f8bf2472bd5afcecb89dfa489
Giovanni7779 <55230060+Giovanni7779@users.noreply.github.com>
2019-09-18 16:55:26 +0200

    Update ex2.py

--- ex2.py
+++ ex2.py
@@ -1,49 +1,69 @@
 
+
 import matplotlib.pyplot as plt
 import math
 import numpy as np
 
 
 def Fourier(S, T, F) :
-
   #[Signal]    #S is the data set
   #[Time]      #T is an array with start time,      finish time,      time intervall
   #[Frequency] #F is an array with start frequency, finish frequency, frequency intervall
-
   Time = np.arange(T[0], T[1], T[2]) ;
   Freq = np.arange(F[0], F[1], F[2]) ;
   FT = [0]*len(Freq) ;
-  for i in range (0, len(FT)-1) : 
+  for i in range (0, len(FT)) : 
      SUM = 0 ; 
-     for t in range (0, len(S) -1) :
-        SUM = SUM + ( S[t]*math.cos(-2*math.pi*Freq[i]*Time[t])*T[2] ) ;
+     for t in range (0, len(S) - 1) :
+        SUM = SUM + ( ( S[t]*math.cos(-2*math.pi*Freq[i]*Time[t]) ) + 1j*(S[t]*math.sin(-2*math.pi*Freq[i]*Time[t]) ) ) ;
      FT[i] = SUM ; 
   return FT ; 
 
+#FUNCTIONS
+
+def Delta(T) :
+  Delta = [0]*int((T[1]- T[0])/T[2]) ;
+  Delta[int(len(Delta)/2)+1] = 2 ; 
+  return Delta ;
+
+def Sine(T,f1,f2) :  
+  Sine = [0]*int((T[1]- T[0])/T[2]) ;
+  Time = np.arange(T[0], T[1], T[2]) ;
+  for i in range (0, len(Sine) - 1) : 
+     Sine[i] = math.sin(Time[i]*f1) + 2*math.sin(Time[i]*f2) ;
+  return Sine; 
+
+def Sawtooth
+
+#---------------------
+
+Time = np.array([0, 1*(10**6) , 10**3     ]) ; 
+Freq = np.array([0, 10000*(10**(-5)), 10000*(10**(-8)) ]) ;
+  
+
+#Creation of the Signals
+
+#DELTA FUNCTION
+Delta = Delta(Time) ;
+Sine = Sine (Time, 2*math.pi*(10**(-5)), 0 ) ; 
 
 
 
-Time = np.array([0, 0.6*(10**6) , 10**3     ]) ; 
-Freq = np.array([0, 5*(10**(-5)), 5*10**(-8)]) ;
+Frequency = np.arange(Freq[0], Freq[1], Freq[2]) ;  #This helps with the graphs
+Tempo = np.arange(Time[0], Time[1], Time[2]) ;  #This helps with the graphs
+FT = Fourier(Sine, Time, Freq) ;
 
-
-
-#Creation of the Signal
-Signal = [0]*int((Time[1]- Time[0])/Time[2]);
-Signal[int(len(Signal)/2)-1] = 1; 
-Signal[int(len(Signal)/2)-1] = 1; 
 
 fig = plt.figure(figsize=(20,12))
-plt.plot(Signal,'r+')
-plt.plot(Signal)
+plt.plot(Tempo, Sine , 'r+')
+plt.plot(Tempo, Sine )
 plt.grid(True)
 plt.show()
 
-Frequency = np.arange(Freq[0], Freq[1], Freq[2]) ;  #This helps with the graphs
-FT = Fourier(Signal, Time, Freq) ;
-
 fig = plt.figure(figsize=(20,12))
-plt.plot(Frequency, np.power(FT,2) , 'r+')
-plt.plot(Frequency, np.power(FT,2) )
+plt.plot(Frequency, np.abs(FT) , 'r+')
+plt.plot(Frequency, np.abs(FT) )
+plt.xlabel('Frequency 1/[year]')
+plt.ylabel('Magnitude [not normalize]')
 plt.grid(True)
 plt.show()
